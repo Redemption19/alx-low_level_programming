d8b43a8b9102ed841ff7c4adc5a8546fa925657a
Bismark Abban <abbanb2@gmail.com>
2022-06-30 10:23:51 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * 
- * Description: 'Hello, puts C program'
- * 
- * Return: Always 0 (Success)
+ * Description: 'Hello, puts C Program'
+ * Return: Always 0 (sucess)
  */
-
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
